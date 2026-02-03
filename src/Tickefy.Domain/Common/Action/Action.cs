d5b6d4109b1b18ba53c70cef4b1d984efa378baa
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-02-03 22:00:12 +0200

    feat: ticket actions

---
+++ src/Tickefy.Domain/Common/Action/Action.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Tickefy.Domain.Common.Action;
+
+public enum Action
+{
+    Cancel,
+    Take,
+    Start, 
+    Accept,
+    Reopen,
+    Publish,
+    Complete, 
+    Fail
+}
