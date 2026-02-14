f314008c52420ad48bbcbb8ac2273aefa8aa8963
khomiak-denys <denus.homiak@gmail.com>
2026-02-14 15:09:36 +0200

    feat: CancelTicketCommand handler

---
+++ tests/Tickefy.Application.Tests/Tickets/CancelTicketHandler.cs
@@ -0,0 +1,80 @@
+using Moq;
+using Tickefy.Application.Abstractions.Data;
+using Tickefy.Application.Exceptions;
+using Tickefy.Application.Ticket.Cancel;
+using Tickefy.Domain.Common.Event;
+using Tickefy.Domain.ActivityLog;
+using Tickefy.Domain.Primitives;
+using Tickefy.Domain.Ticket;
+
+namespace Tickefy.Application.Tests.Tickets;
+
+public class CancelTicketHandler
+{
+    [Fact]
+    public async Task CancelTicketCommandHandler_Should_Throw_NotFoundException_On_Null_Ticket()
+    {
+        var repo = new Mock<ITicketRepository>();
+        repo.Setup(r => r.GetByIdAsync(It.IsAny<TicketId>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Domain.Ticket.Ticket?)null);
+
+        var logRepository = new Mock<IActivityLogRepository>();
+        var uow = new Mock<IUnitOfWork>();
+        
+        var handler = new CancelTicketCommandHandler(repo.Object, logRepository.Object, uow.Object);
+
+        var command = new CancelTicketCommand(new UserId(), new List<string>(), new TicketId(), string.Empty);
+        var func = () => handler.Handle(command, CancellationToken.None);
+        
+        await func.Should().ThrowAsync<NotFoundException>();
+    }
+    
+    [Fact]
+    public async Task CancelTicketCommandHandler_Should_Throw_ForBiddenException_On_AgentRole()
+    {
+        var repo = new Mock<ITicketRepository>();
+        repo.Setup(r => r.GetByIdAsync(It.IsAny<TicketId>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Domain.Ticket.Ticket.Create(string.Empty, string.Empty, new UserId(), DateTime.UtcNow));
+
+        var logRepository = new Mock<IActivityLogRepository>();
+        var uow = new Mock<IUnitOfWork>();
+        
+        var handler = new CancelTicketCommandHandler(repo.Object, logRepository.Object, uow.Object);
+
+        var command = new CancelTicketCommand(new UserId(), ["Agent"], new TicketId(), string.Empty);
+        var func = () => handler.Handle(command, CancellationToken.None);
+        
+        await func.Should().ThrowAsync<ForbiddenException>();
+    }
+
+    [Fact]
+    public async Task CancelTicketCommandHandler_Should_CancelTicketAndLogReason_WhenConditionAllows()
+    {
+        var ticket = Domain.Ticket.Ticket.Create(string.Empty, string.Empty, new UserId(), DateTime.UtcNow);
+        var ticketId = ticket.Id;
+        var userId = new UserId();
+        var eventType = EventType.StatusChanged;
+        var reason = "Test description";
+        
+        var repo = new Mock<ITicketRepository>();
+        repo.Setup(r => r.GetByIdAsync(It.IsAny<TicketId>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(ticket);
+
+        var logRepository = new Mock<IActivityLogRepository>();
+        var uow = new Mock<IUnitOfWork>();
+        
+        var handler = new CancelTicketCommandHandler(repo.Object, logRepository.Object, uow.Object);
+
+        var command = new CancelTicketCommand(userId, ["Admin"], ticketId, reason);
+        await handler.Handle(command, CancellationToken.None);
+
+        logRepository.Verify(repo => repo.Add(It.Is<Domain.ActivityLog.ActivityLog>(log =>
+            log.UserId == userId &&
+                log.TicketId == ticketId &&
+                log.EventType == eventType &&
+                log.Description.Contains(reason)
+        )), Times.Once);
+        
+        uow.Verify(uow => uow.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
