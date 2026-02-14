ca6d84be6088b3283edb530ff65aa4b5fca20ff3
khomiak-denys <denus.homiak@gmail.com>
2026-02-14 15:09:14 +0200

    refactor: CancelTicketCommand + handler

--- src/Tickefy.Application/Ticket/Cancel/CancelTicketCommandHandler.cs
+++ src/Tickefy.Application/Ticket/Cancel/CancelTicketCommandHandler.cs
@@ -2,10 +2,10 @@
 using Tickefy.Application.Abstractions.Data;
 using Tickefy.Application.Abstractions.Messaging;
 using Tickefy.Application.Exceptions;
+using Tickefy.Application.Ticket.Common.Helpers;
 using Tickefy.Domain.ActivityLog;
+using Tickefy.Domain.Common.Action;
 using Tickefy.Domain.Common.Event;
-using Tickefy.Domain.Common.Status;
-using Tickefy.Domain.Common.UserRole;
 using Tickefy.Domain.Ticket;
 
 namespace Tickefy.Application.Ticket.Cancel
@@ -31,22 +31,12 @@ namespace Tickefy.Application.Ticket.Cancel
 
             if (ticket == null) throw new NotFoundException(nameof(ticket), command.TicketId);
 
-            var isRequester = command.Roles.Contains(UserRoles.Requester.ToString()) && (ticket.RequesterId.Value == command.UserId.Value);
-            var isAdmin = command.Roles.Contains(UserRoles.Admin.ToString());
-
-            if (isAdmin || isRequester)
+            if (TicketAction.Cancel.CanExecute(ticket, command.UserId, command.Roles))
             {
-                if (ticket.Status == Status.Created)
-                {
                     ticket.Cancel();
-                    var log = Domain.ActivityLog.ActivityLog.Create(ticket.Id, command.UserId, EventType.StatusChanged, "Ticket canceled");
+                    var log = Domain.ActivityLog.ActivityLog.Create(ticket.Id, command.UserId, EventType.StatusChanged, $"Ticket canceled. Reason: {command.Reason}");
                     _logRepository.Add(log);
-                    await _uow.SaveChangesAsync();
-                }
-                else
-                {
-                    throw new ForbiddenException($"Ticket status must be 'Created' to cancel. Current status {ticket.Status}");
-                }
+                    await _uow.SaveChangesAsync(cancellationToken);
             }
             else
             {
