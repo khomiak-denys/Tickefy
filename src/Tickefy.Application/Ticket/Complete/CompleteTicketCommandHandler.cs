eefd0eeb5f7d6169ae43ab0b2e3537a3c3536b1d
khomiak-denys <denus.homiak@gmail.com>
2026-02-16 00:18:27 +0200

    refactor: complete ticket command handler

--- src/Tickefy.Application/Ticket/Complete/CompleteTicketCommandHandler.cs
+++ src/Tickefy.Application/Ticket/Complete/CompleteTicketCommandHandler.cs
@@ -2,7 +2,9 @@
 using Tickefy.Application.Abstractions.Data;
 using Tickefy.Application.Abstractions.Messaging;
 using Tickefy.Application.Exceptions;
+using Tickefy.Application.Ticket.Common.Helpers;
 using Tickefy.Domain.ActivityLog;
+using Tickefy.Domain.Common.Action;
 using Tickefy.Domain.Common.Event;
 using Tickefy.Domain.Common.Status;
 using Tickefy.Domain.Common.UserRole;
@@ -31,22 +33,14 @@ namespace Tickefy.Application.Ticket.Complete
 
             if (ticket == null) throw new NotFoundException(nameof(ticket), command.TicketId);
 
-            var isAssignedAgent = command.Roles.Contains(UserRoles.Agent.ToString()) && (ticket.AssignedAgentId?.Value == command.UserId.Value); 
-            var isAdmin = command.Roles.Contains(UserRoles.Admin.ToString());
-
-            if (isAdmin || isAssignedAgent)
+            if (TicketAction.Complete.CanExecute(ticket, command.UserId, command.Roles))
             {
-                if (ticket.Status == Status.Assigned)
-                {
-                    ticket.Complete();
-                    var log = Domain.ActivityLog.ActivityLog.Create(ticket.Id, command.UserId, EventType.StatusChanged, "Ticket completed");
-                    _logRepository.Add(log);
-                    await _uow.SaveChangesAsync();
-                }
-                else
-                {
-                    throw new ForbiddenException($"Ticket status must be 'Assigned' to complete. Current status {ticket.Status}");
-                }
+                ticket.Complete();
+                var log = Domain.ActivityLog.ActivityLog.Create(ticket.Id, command.UserId, EventType.StatusChanged,
+                    "Ticket completed");
+                _logRepository.Add(log);
+                await _uow.SaveChangesAsync();
+
             }
             else
             {
---
+++ tests/Tickefy.Application.Tests/Tickets/CompleteTicketHandler.cs
@@ -0,0 +1,94 @@
+using Moq;
+using Tickefy.Application.Abstractions.Data;
+using Tickefy.Application.Exceptions;
+using Tickefy.Application.Ticket.Complete;
+using Tickefy.Domain.ActivityLog;
+using Tickefy.Domain.Common.Event;
+using Tickefy.Domain.Primitives;
+using Tickefy.Domain.Ticket;
+
+namespace Tickefy.Application.Tests.Tickets;
+
+public class CompleteTicketHandler
+{
+    [Fact]
+    public async Task CompleteTicketCommandHandler_Should_Throw_NotFoundException_On_Null_Ticket()
+    {
+        var repo = new Mock<ITicketRepository>();
+        repo.Setup(r => r.GetByIdAsync(It.IsAny<TicketId>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Domain.Ticket.Ticket?)null);
+
+        var logRepository = new Mock<IActivityLogRepository>();
+        var uow = new Mock<IUnitOfWork>();
+        
+        var handler = new CompleteTicketCommandHandler(repo.Object, logRepository.Object, uow.Object);
+
+        var command = new CompleteTicketCommand{
+            UserId = new UserId(), 
+            Roles = new List<string>(), 
+            TicketId = new TicketId()
+        };
+        
+        var func = () => handler.Handle(command, CancellationToken.None);
+        
+        await func.Should().ThrowAsync<NotFoundException>();
+    }
+    
+    [Fact]
+    public async Task CompleteTicketCommandHandler_Should_Throw_ForBiddenException_On_RequesterRole()
+    {
+        var repo = new Mock<ITicketRepository>();
+        repo.Setup(r => r.GetByIdAsync(It.IsAny<TicketId>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Domain.Ticket.Ticket.Create(string.Empty, string.Empty, new UserId(), DateTime.UtcNow));
+
+        var logRepository = new Mock<IActivityLogRepository>();
+        var uow = new Mock<IUnitOfWork>();
+        
+        var handler = new CompleteTicketCommandHandler(repo.Object, logRepository.Object, uow.Object);
+
+        var command = new CompleteTicketCommand {
+            UserId = new UserId(), 
+            Roles = ["Requester"], 
+            TicketId = new TicketId()
+        };
+        var func = () => handler.Handle(command, CancellationToken.None);
+        
+        await func.Should().ThrowAsync<ForbiddenException>();
+    }
+    
+    [Fact]
+    public async Task CompleteTicketCommandHandler_Should_CompleteTicketAndLogReason_WhenConditionAllows()
+    {
+        var ticket = Domain.Ticket.Ticket.Create(string.Empty, string.Empty, new UserId(), DateTime.UtcNow);
+        var ticketId = ticket.Id;
+        var agentId = new UserId();
+        var eventType = EventType.StatusChanged;
+        
+        ticket.Take(agentId, new TeamId());
+        ticket.StartWork();
+        
+        var repo = new Mock<ITicketRepository>();
+        repo.Setup(r => r.GetByIdAsync(It.IsAny<TicketId>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(ticket);
+
+        var logRepository = new Mock<IActivityLogRepository>();
+        var uow = new Mock<IUnitOfWork>();
+        
+        var handler = new CompleteTicketCommandHandler(repo.Object, logRepository.Object, uow.Object);
+
+        var command = new CompleteTicketCommand {
+            UserId = agentId, 
+            Roles = ["Agent"], 
+            TicketId = ticketId
+        };
+        await handler.Handle(command, CancellationToken.None);
+
+        logRepository.Verify(repo => repo.Add(It.Is<Domain.ActivityLog.ActivityLog>(log =>
+            log.UserId == agentId &&
+            log.TicketId == ticketId &&
+            log.EventType == eventType 
+        )), Times.Once);
+        
+        uow.Verify(uow => uow.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
