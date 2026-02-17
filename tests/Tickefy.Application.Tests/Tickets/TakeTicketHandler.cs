edfe31935b986fef42d2842be758d3b5c3f89d2e
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-02-17 13:29:55 +0200

    feat: take ticket handler tests

--- tests/Tickefy.Application.Tests/Tickets/TakeTicketHandler.cs
+++ tests/Tickefy.Application.Tests/Tickets/TakeTicketHandler.cs
@@ -80,7 +80,7 @@ public class TakeTicketHandler
     }
 
     [Fact]
-    public async Task TakeTicketCommandHandler_Should_Throw_NotFoundException_On_Null_Requester()
+    public async Task TakeTicketCommandHandler_Should_Throw_NotFoundException_On_Null_Agent()
     {
         var ticket = Domain.Ticket.Ticket.Create(string.Empty, string.Empty, new UserId(), DateTime.UtcNow);
         
@@ -114,7 +114,7 @@ public class TakeTicketHandler
     }
     
     [Fact]
-    public async Task TakeTicketCommandHandler_Should_Throw_NotFoundException_When_Requester_TeamId_IsNull()
+    public async Task TakeTicketCommandHandler_Should_Throw_ForbiddenException_When_Agent_TeamId_IsNull()
     {
         var user = Domain.User.User.Create(string.Empty, string.Empty, string.Empty, string.Empty);
         var team = Domain.Team.Team.Create(string.Empty, string.Empty);
@@ -142,6 +142,48 @@ public class TakeTicketHandler
 
         var command = new TakeTicketCommand {
             UserId = new UserId(), 
+            Roles = [nameof(UserRoles.Agent)], 
+            TicketId = new TicketId()
+        };
+        var func = () => handler.Handle(command, CancellationToken.None);
+        
+        await func.Should().ThrowAsync<ForbiddenException>();
+    }
+
+    [Fact]
+    public async Task TakeTicketCommandHandler_Should_Throw_NotFoundException_When_Agent_Team_IsNotFound()
+    {
+        var user = Domain.User.User.Create(string.Empty, string.Empty, string.Empty, string.Empty);
+        var team = Domain.Team.Team.Create(string.Empty, string.Empty);
+        
+        team.AddMember(user);
+        
+        var ticket = Domain.Ticket.Ticket.Create(string.Empty, string.Empty, new UserId(), DateTime.UtcNow);
+        
+        var ticketRepository = new Mock<ITicketRepository>();
+        ticketRepository.Setup(r => r.GetByIdAsync(It.IsAny<TicketId>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(ticket);
+
+        var userRepository = new Mock<IUserRepository>();
+        userRepository.Setup(r => r.GetByIdAsync(It.IsAny<UserId>()))
+            .ReturnsAsync(user);
+        
+        var teamRepository = new Mock<ITeamRepository>();
+        teamRepository.Setup(r => r.GetByIdAsync(It.IsAny<TeamId>()))
+            .ReturnsAsync((Domain.Team.Team?)null);
+        
+        var logRepository = new Mock<IActivityLogRepository>();
+        var uow = new Mock<IUnitOfWork>();
+        
+        var handler = new TakeTicketCommandHandler(
+            ticketRepository.Object, 
+            logRepository.Object, 
+            userRepository.Object,  
+            teamRepository.Object,
+            uow.Object);
+
+        var command = new TakeTicketCommand {
+            UserId = user.Id, 
             Roles = [nameof(UserRoles.Agent)], 
             TicketId = new TicketId()
         };
@@ -149,13 +191,13 @@ public class TakeTicketHandler
         
         await func.Should().ThrowAsync<NotFoundException>();
     }
-
+    
     [Fact]
     public async Task TakeTicketCommandHandler_Should_AssignTicket_And_Log()
     {
         var user = Domain.User.User.Create(string.Empty, string.Empty, string.Empty, string.Empty);
         var team = Domain.Team.Team.Create(string.Empty, string.Empty);
-        var teamId = team.Id;
+        team.AddMember(user);
         
         var ticket = Domain.Ticket.Ticket.Create(string.Empty, string.Empty, new UserId(), DateTime.UtcNow);
         
@@ -167,6 +209,8 @@ public class TakeTicketHandler
         userRepository.Setup(r => r.GetByIdAsync(It.IsAny<UserId>()))
             .ReturnsAsync(user);
         var teamRepository = new Mock<ITeamRepository>();
+        teamRepository.Setup(r => r.GetByIdAsync(It.IsAny<TeamId>()))
+            .ReturnsAsync(team);
         
         var logRepository = new Mock<IActivityLogRepository>();
         var uow = new Mock<IUnitOfWork>();
@@ -179,12 +223,20 @@ public class TakeTicketHandler
             uow.Object);
 
         var command = new TakeTicketCommand {
-            UserId = new UserId(), 
+            UserId = user.Id, 
             Roles = [nameof(UserRoles.Agent)], 
             TicketId = new TicketId()
         };
-        var func = () => handler.Handle(command, CancellationToken.None);
         
-        await func.Should().ThrowAsync<NotFoundException>();
+        await handler.Handle(command, CancellationToken.None);
+        
+        logRepository.Verify(repo => repo.Add(It.Is<Domain.ActivityLog.ActivityLog>(log => 
+            log.UserId ==  user.Id && 
+            log.TicketId == ticket.Id && 
+            log.EventType == EventType.UserAssigned && 
+            log.Description != string.Empty
+            )));
+        
+        uow.Verify(uow => uow.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
     } 
 }
