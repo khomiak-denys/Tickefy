7d7e9636299ce5fd553d08508d0f05bdc2c8241c
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-03-05 14:15:57 +0200

    refactor(errors): add result interface

---
+++ src/Tickefy.Domain/Common/Resultor/IResult.cs
@@ -0,0 +1,19 @@
+﻿using Tickefy.Domain.Common.Errors;
+
+namespace Tickefy.Domain.Common.Resultor;
+
+public interface IResult<out T>
+{
+    public T Value { get; }
+    public Error Error { get; }
+    public bool IsSuccess { get; }
+    public bool IsFailure { get; }
+}
+
+public interface IResult
+{
+    public Error Error { get; }
+    public bool IsSuccess { get; }
+    public bool IsFailure { get; }
+}
+
