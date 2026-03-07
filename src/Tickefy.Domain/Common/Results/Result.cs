17772ea8a18451f8e5653f18816204619d6ac476
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-03-07 13:42:09 +0200

    fix(result-pattern): remove implicit operators, add failure & success methods

--- src/Tickefy.Domain/Common/Results/Result.cs
+++ src/Tickefy.Domain/Common/Results/Result.cs
@@ -29,11 +29,6 @@ public class Result : IResult
     {
         return new Result(error);
     }
-
-    public static implicit operator Result(Error error)
-    {
-        return new Result(error);   
-    }
 }
 
 public class Result<T> : IResult<T>
@@ -57,13 +52,13 @@ public class Result<T> : IResult<T>
         Error = error;
         Value = default(T)!;
     }
-
-    public static implicit operator Result<T>(T value)
+    
+    public static Result<T> Success(T value)
     {
         return new Result<T>(value);
     }
 
-    public static implicit operator Result<T>(Error error)
+    public static Result<T> Failure(Error error)
     {
         return new Result<T>(error);
     }
