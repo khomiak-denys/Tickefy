169568c2d44debedb8595ac1c907178abc504442
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-03-07 14:46:33 +0200

    feat(result-pattern): add result match extension

---
+++ src/Tickefy.API/ErrorHandling/ResultExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tickefy.Domain.Common.Errors;
+using Tickefy.Domain.Common.Results;
+
+namespace Tickefy.API.ErrorHandling;
+
+public static class ResultExtensions
+{
+    public static IActionResult Match(this Result resultor, IActionResult result, Func<Error, IActionResult> onFailure)
+    {
+        return resultor.IsSuccess ?  result : onFailure(resultor.Error);
+    }
+}
