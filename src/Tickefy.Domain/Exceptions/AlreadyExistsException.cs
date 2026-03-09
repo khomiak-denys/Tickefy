f3eb97a9f25cd458ce57ac4f10f172876d9ad196
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-03-09 16:38:31 +0200

    refactor(exceptions): remove unused exceptions & mapping from exceptionMapper

--- src/Tickefy.Domain/Exceptions/AlreadyExistsException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tickefy.Application.Exceptions
-{
-    public class AlreadyExistsException : Exception
-    {
-        public AlreadyExistsException(string message) : base(message) { }
-        public AlreadyExistsException(string name, object key)
-            : base($"Entity \"{name}\" ({key}) already exists.") { }
-    }
-}
--- src/Tickefy.Domain/Exceptions/ForbiddenException.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Tickefy.Application.Exceptions
-{
-    public class ForbiddenException : Exception
-    {
-        public ForbiddenException(string message) : base(message) { }
-    }
-}
