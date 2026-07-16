bdc3c7c6a429266068d6b2f8a7f02e0105e84836
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-07-16 13:41:59 +0300

    test

--- tests/Tickefy.Application.Tests/Mapping/Users/UserMappingTests.cs
+++ tests/Tickefy.Application.Tests/Mapping/Users/UserMappingTests.cs
@@ -30,7 +30,7 @@ public class UserMappingTests
 
         // Assert
         result.Should().NotBeNull();
-        result.Id.Should().Be(Guid.Empty);
+        result.Id.Should().Be(1);
         result.FirstName.Should().BeEmpty();
         result.LastName.Should().BeEmpty();
     }
